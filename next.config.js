dbde3f3ec68a140b49478eac558d8a1506f94601
Praveen Juge <hello@praveenjuge.com>
2023-11-26 10:49:32 +0530

    Fixes
    Signed-off-by: Praveen Juge <hello@praveenjuge.com>

--- next.config.js
+++ next.config.js
@@ -36,7 +36,7 @@ const nextConfig = {
           {
             key: 'Content-Security-Policy',
             value:
-              "default-src 'self'; frame-src 'self' https://www.youtube-nocookie.com/; font-src 'self'; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none';script-src 'self' 'unsafe-eval' 'unsafe-inline'; img-src 'self' blob: data:; connect-src 'self'; style-src 'self' 'unsafe-inline'; block-all-mixed-content; upgrade-insecure-requests;"
+              "default-src 'self'; frame-src 'self' https://www.youtube-nocookie.com/; font-src 'self'; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none';script-src 'self' 'unsafe-eval' 'unsafe-inline'; img-src 'self' blob: https://avatars.githubusercontent.com; connect-src 'self' https://api.github.com/graphql; style-src 'self' 'unsafe-inline'; block-all-mixed-content; upgrade-insecure-requests;"
           }
         ]
       }
