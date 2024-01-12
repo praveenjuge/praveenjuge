6653162fe90b3ddf8649b38c72ead7d472a918c9
Praveen Juge <hello@praveenjuge.com>
2024-01-12 11:35:24 +0530

    RSS Fixes
    Signed-off-by: Praveen Juge <hello@praveenjuge.com>

--- app/blog/rss.xml/route.ts
+++ app/blog/rss.xml/route.ts
@@ -1,8 +1,10 @@
+import { Feed } from 'feed';
 import { load } from 'outstatic/server';
-import Rss from 'rss';
 
 export const dynamic = 'force-static';
 const SITE_URL = 'https://praveenjuge.com/';
+const SITE_AUTHOR_NAME = 'Praveen Juge';
+const SITE_AUTHOR_EMAIL = 'hi@praveenjuge.com';
 
 export async function GET() {
   const db = await load();
@@ -12,32 +14,59 @@ export async function GET() {
       'title',
       'publishedAt',
       'description',
-      'slug'
+      'content',
+      'slug',
+      'coverImage'
     ])
     .sort({ publishedAt: -1 })
     .toArray();
 
-  const feed = new Rss({
-    title: 'Blog Posts | Praveen Juge',
+  const feed = new Feed({
+    title: SITE_AUTHOR_NAME,
     description:
       'Praveen Juge is a designer and developer for everything on the web.',
-    image_url: 'https://praveenjuge.com/favicon.ico',
-    feed_url: 'https://praveenjuge.com/blog/index.xml',
-    site_url: SITE_URL,
-    language: 'en-us'
+    id: SITE_URL,
+    link: SITE_URL,
+    language: 'en',
+    favicon: `${SITE_URL}favicon.ico`,
+    copyright: SITE_AUTHOR_NAME,
+    author: {
+      name: SITE_AUTHOR_NAME,
+      email: SITE_AUTHOR_EMAIL,
+      link: SITE_URL
+    }
   });
 
   allBlogs.forEach((post) => {
-    feed.item({
+    const url = `${SITE_URL}blog/${post.slug}`;
+    feed.addItem({
       title: post.title || '',
+      id: url,
+      link: url,
       description: post.description || '',
-      url: `${SITE_URL}blog/${post.slug}`,
-      guid: `${SITE_URL}blog/${post.slug}`,
-      date: post.publishedAt || ''
+      content: post.content,
+      date: new Date(post.publishedAt || ''),
+      image: post.coverImage,
+      author: [
+        {
+          name: SITE_AUTHOR_NAME,
+          email: SITE_AUTHOR_EMAIL,
+          link: SITE_URL
+        }
+      ]
     });
   });
 
-  return new Response(feed.xml(), {
+  feed.addCategory('Design');
+  feed.addCategory('Technology');
+
+  feed.addContributor({
+    name: SITE_AUTHOR_NAME,
+    email: SITE_AUTHOR_EMAIL,
+    link: SITE_URL
+  });
+
+  return new Response(feed.rss2(), {
     headers: {
       'Content-Type': 'application/xml'
     }
