8f0748938d2f5a0ecd3cf8607d5110bbf6752aa3
Praveen Juge <hello@praveenjuge.com>
2024-01-15 10:38:48 +0530

    Fixes
    Signed-off-by: Praveen Juge <hello@praveenjuge.com>

--- app/design/page.tsx
+++ app/design/page.tsx
@@ -14,28 +14,31 @@ export default async function Page() {
     .toArray();
 
   return (
-    <section className="mx-auto max-w-3xl p-4">
+    <section className="mx-auto max-w-7xl p-4">
       <h2 className="my-6 text-2xl font-extrabold tracking-tight md:text-3xl">
         Designs
       </h2>
 
-      {allDesigns.map((item, id) => (
-        <Link
-          key={id}
-          target="_blank"
-          className="my-10 block"
-          href={item.coverImage ?? ''}
-        >
-          <Image
-            width={700}
-            height={800}
-            priority={id === 0}
-            alt={item.coverImage ?? ''}
-            src={item.coverImage ?? ''}
-            className="h-full w-full rounded bg-slate-200 bg-cover ring-1 ring-slate-100"
-          />
-        </Link>
-      ))}
+      <div className="grid grid-cols-1 gap-10 md:grid-cols-2">
+        {allDesigns.map((item, id) => (
+          <Link
+            key={id}
+            target="_blank"
+            className="block"
+            href={item.coverImage ?? ''}
+          >
+            <Image
+              width={700}
+              height={800}
+              priority={id === 0}
+              loading={id > 10 ? 'lazy' : 'eager'}
+              alt={item.coverImage ?? ''}
+              src={item.coverImage ?? ''}
+              className="h-full w-full rounded bg-slate-200 bg-cover bg-center ring-1 ring-slate-100"
+            />
+          </Link>
+        ))}
+      </div>
     </section>
   );
 }
