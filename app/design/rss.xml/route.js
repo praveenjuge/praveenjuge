6d5994d5cecf71e4be0911760108cd192683314e
Praveen Juge <hello@praveenjuge.com>
2024-10-08 19:49:29 +0530

    Move RSS to Custom
    Signed-off-by: Praveen Juge <hello@praveenjuge.com>

--- app/design/rss.xml/route.js
+++ app/design/rss.xml/route.js
@@ -1,65 +1,62 @@
-import { Feed } from "feed";
 import { load } from "outstatic/server";
+import { NextResponse } from 'next/server';
 
 export const dynamic = "force-static";
-const SITE_URL = "https://praveenjuge.com/";
-const SITE_AUTHOR_NAME = "Praveen Juge";
-const SITE_AUTHOR_EMAIL = "hi@praveenjuge.com";
+const SITE_URL = 'https://praveenjuge.com';
+const AUTHOR_NAME = 'Praveen Juge';
+const AUTHOR_EMAIL = 'hi@praveenjuge.com';
+const FEED_ICON = `${SITE_URL}/favicon.ico`;
+const FEED_SELF_LINK = `${SITE_URL}/design/rss.xml`;
+const FEED_SUBTITLE = 'Praveen Juge is a designer and developer for everything on the web.';
 
 const allDesigns = await (await load())
 	.find({ collection: "design" }, ["title", "publishedAt", "slug"])
 	.sort({ publishedAt: -1 })
 	.toArray();
+	
+export async function GET() {
+	const feed = `<feed xmlns="http://www.w3.org/2005/Atom">
+			<id>${SITE_URL}/</id>
+			<title>${AUTHOR_NAME}</title>
+			<updated>${new Date().toISOString()}</updated>
+			<author>
+				<name>${AUTHOR_NAME}</name>
+				<email>${AUTHOR_EMAIL}</email>
+				<uri>${SITE_URL}/</uri>
+			</author>
+			<link rel="alternate" href="${SITE_URL}/"/>
+			<link rel="self" href="${FEED_SELF_LINK}"/>
+			<subtitle>${FEED_SUBTITLE}</subtitle>
+			<icon>${FEED_ICON}</icon>
+			<rights>${AUTHOR_NAME}</rights>
+			<category term="Design"/>
+			<category term="Technology"/>
+			<contributor>
+				<name>${AUTHOR_NAME}</name>
+				<email>${AUTHOR_EMAIL}</email>
+				<uri>${SITE_URL}/</uri>
+			</contributor>
+			${allDesigns
+				.map(
+					(entry) => `
+					<entry>
+						<title type="html"><![CDATA[ ${entry.title} ]]></title>
+						<id>${SITE_URL}/design/${entry.slug}</id>
+						<link href="${SITE_URL}/design/${entry.slug}"/>
+						<updated>${new Date(entry.publishedAt).toISOString()}</updated>
+						<author>
+							<name>${AUTHOR_NAME}</name>
+							<email>${AUTHOR_EMAIL}</email>
+							<uri>${SITE_URL}/</uri>
+						</author>
+					</entry>`
+				)
+				.join('')}
+		</feed>`;
 
-export function GET() {
-	const feed = new Feed({
-		title: SITE_AUTHOR_NAME,
-		description:
-			"Praveen Juge is a designer and developer for everything on the web.",
-		id: SITE_URL,
-		link: SITE_URL,
-		language: "en",
-		favicon: `${SITE_URL}favicon.ico`,
-		copyright: SITE_AUTHOR_NAME,
-		feedLinks: {
-			atom: `${SITE_URL}design/rss.xml`,
-		},
-		author: {
-			name: SITE_AUTHOR_NAME,
-			email: SITE_AUTHOR_EMAIL,
-			link: SITE_URL,
-		},
-	});
-
-	for (const post of allDesigns) {
-		const url = `${SITE_URL}design/${post.slug}`;
-		feed.addItem({
-			title: post.title || "",
-			id: url,
-			link: url,
-			date: new Date(post.publishedAt || ""),
-			author: [
-				{
-					name: SITE_AUTHOR_NAME,
-					email: SITE_AUTHOR_EMAIL,
-					link: SITE_URL,
-				},
-			],
-		});
-	}
-
-	feed.addCategory("Design");
-	feed.addCategory("Technology");
-
-	feed.addContributor({
-		name: SITE_AUTHOR_NAME,
-		email: SITE_AUTHOR_EMAIL,
-		link: SITE_URL,
-	});
-
-	return new Response(feed.atom1(), {
+	return new NextResponse(feed, {
 		headers: {
-			"Content-Type": "application/xml",
+			'Content-Type': 'application/atom+xml',
 		},
 	});
 }
