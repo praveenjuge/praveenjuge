d507fcce9026534b1927024524af217b04a4dc9e
Praveen Juge <hello@praveenjuge.com>
2024-10-14 00:53:55 +0530

    Fixes
    Signed-off-by: Praveen Juge <hello@praveenjuge.com>

--- app/layout.jsx
+++ app/layout.jsx
@@ -173,7 +173,6 @@ export default function RootLayout({ children }) {
 				</footer>
 				<script
 					type="application/ld+json"
-					// biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
 					dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
 				/>
 			</body>
