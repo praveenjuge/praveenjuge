8498048adb74c8045233703b7dc944d7ea59ec40
Praveen Juge <hello@praveenjuge.com>
2024-10-31 16:38:29 +0530

    Astro Time
    Signed-off-by: Praveen Juge <hello@praveenjuge.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite'
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
     site: 'https://astro.praveenjuge.com',
     integrations: [sitemap()],
