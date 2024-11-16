cbd2fa3f2c3542ce53a087880580353b5a4c2923
Praveen Juge <hello@praveenjuge.com>
2024-11-16 11:42:36 +0530

    Inline CSS
    Signed-off-by: Praveen Juge <hello@praveenjuge.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite'
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-    site: 'https://praveenjuge.com',
-    prefetch: {
-        prefetchAll: true,
-        defaultStrategy: 'load'
-    },
-    trailingSlash: 'never',
-    integrations: [sitemap()],
-    vite: { plugins: [tailwindcss()] }
+  site: "https://praveenjuge.com",
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "load",
+  },
+  build: {
+    inlineStylesheets: "always",
+  },
+  trailingSlash: "never",
+  integrations: [sitemap()],
+  vite: { plugins: [tailwindcss()] },
 });
