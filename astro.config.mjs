4fe780d3aa9fe19afad123efa29c60753751bc3f
Praveen Juge <hello@praveenjuge.com>
2024-11-30 10:35:17 +0530

    Fixes
    Signed-off-by: Praveen Juge <hello@praveenjuge.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,6 @@ export default defineConfig({
   build: {
     inlineStylesheets: "always",
   },
-  trailingSlash: "never",
   integrations: [sitemap()],
   vite: { plugins: [tailwindcss()] },
 });
